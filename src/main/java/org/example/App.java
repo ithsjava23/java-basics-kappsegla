baebdaa00e90dffbd8ad16455a0f6986c3751158
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-15 13:23:44 +0200

    Add code for printing menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,19 @@ package org.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+        String menu = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                """;
+
+        System.out.print(menu);
+
+
     }
 }
